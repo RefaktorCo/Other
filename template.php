6054e1a90ec213d45f215edd6e3923eac12c4d50
refaktorco <refaktor.co@gmail.com>
2013-11-11 15:50:48 -0800

    -More basic stuff.

--- template.php
+++ template.php
@@ -1,3 +1,80 @@
 <?php
 
+/**
+ * Overrides theme_process_page().
+ */
+function other_process_page(&$variables) {	
+  // Assign site name and slogan toggle theme settings to variables.
+  $variables['disable_site_name']   = theme_get_setting('toggle_name') ? FALSE : TRUE;
+  $variables['disable_site_slogan'] = theme_get_setting('toggle_slogan') ? FALSE : TRUE;
+   // Assign site name/slogan defaults if there is no value.
+  if ($variables['disable_site_name']) {
+    $variables['site_name'] = filter_xss_admin(variable_get('site_name', 'Drupal'));
+  }
+  if ($variables['disable_site_slogan']) {
+    $variables['site_slogan'] = filter_xss_admin(variable_get('site_slogan', ''));
+  }
+}	
+
+/**
+ * Overrides theme_preprocess_username().
+ */
+function other_preprocess_username(&$vars) {
+  global $theme_key;
+  $theme_name = $theme_key;
+  
+  // Add rel=author for SEO and supporting search engines
+  if (isset($vars['link_path'])) {
+    $vars['link_attributes']['rel'][] = 'author';
+  }
+  else {
+    $vars['attributes_array']['rel'][] = 'author';
+  }
+}
+
+/**
+ * Overrides theme_menu_link().
+ */
+function other_menu_link(array $variables) {
+  //unset all the classes
+  unset($variables['element']['#attributes']['class']);
+  $element = $variables['element'];
+  $sub_menu = '';
+  
+  if($variables['element']['#attributes']) {
+    $sub_menu = '';
+  }
+
+  if ($element['#below']) {
+    $sub_menu = drupal_render($element['#below']);
+  }
+  
+  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+  
+  // if link class is active, make li class as active too
+  if(strpos($output,"active")>0){
+    $element['#attributes']['class'][] = "current-menu-item";
+  }
+  
+  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+}
+
+/**
+ * Overrides theme_menu_tree().
+ */
+function other_menu_tree(&$variables) {
+  return '<ul>' . $variables['tree'] . '</ul>';
+}
+
+/**
+ * User CSS function. Separate from other_preprocess_html so function can be called directly before </head> tag.
+ */
+function other_user_css() {
+  echo "<!-- User defined CSS -->";
+  echo "<style type='text/css'>";
+  echo theme_get_setting('user_css');
+  echo "</style>";
+  echo "<!-- End user defined CSS -->";	
+}
+
 ?>
---
+++ templates/page.tpl.php
@@ -0,0 +1,80 @@
+<div id="mobile-nav">
+	<i class="icon-reorder"></i>
+	<?php if ($logo): ?>
+		<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+	    <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+	  </a>
+	<?php endif; ?>
+</div>
+
+<header>
+	<div id="logo">
+		<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">
+      <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+    </a>
+	</div>
+	
+	<?php if ($site_name || $site_slogan): ?>
+    <div id="name-and-slogan"<?php if ($disable_site_name && $disable_site_slogan) { print ' class="hidden"'; } ?>>
+
+      <?php if ($site_name): ?>
+        <?php if ($title): ?>
+          <div id="site-name"<?php if ($disable_site_name) { print ' class="hidden"'; } ?>>
+            <strong>
+              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+            </strong>
+          </div>
+        <?php else: /* Use h1 when the content title is empty */ ?>
+          <h1 id="site-name"<?php if ($disable_site_name) { print ' class="hidden"'; } ?>>
+            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+          </h1>
+        <?php endif; ?>
+      <?php endif; ?>
+
+      <?php if ($site_slogan): ?>
+        <div id="site-slogan"<?php if ($disable_site_slogan) { print ' class="hidden"'; } ?>>
+          <?php print $site_slogan; ?>
+        </div>
+      <?php endif; ?>
+
+    </div> <!-- /#name-and-slogan -->
+  <?php endif; ?>  
+	
+	<?php if ($page['site_nav']): ?>
+	  <nav>
+      <?php print render($page['site_nav']); ?>
+	  </nav>
+	<?php endif; ?>
+	
+	<?php if ($page['sidebar']): ?>
+    <?php print render($page['sidebar']); ?>
+	<?php endif; ?>
+</header>
+
+<div class="content">
+	<div id="loader"></div>
+	<?php if ($messages): ?>
+	  <div id="messages">
+	   <?php print $messages; ?>
+	  </div>
+	<?php endif; ?>
+	<?php print render($title_prefix); ?>
+  <?php if ($title): ?>
+    <h1 class="title" id="page-title">
+      <?php print $title; ?>
+    </h1>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+  <?php if ($tabs): ?>
+    <div class="tabs">
+      <?php print render($tabs); ?>
+    </div>
+  <?php endif; ?>
+  <?php print render($page['help']); ?>
+  <?php if ($action_links): ?>
+    <ul class="action-links">
+      <?php print render($action_links); ?>
+    </ul>
+  <?php endif; ?>
+	<?php print render($page['content']); ?>
+</div>		
