5de29f4c02f67967db80f5e3e5604c7125cdcfee
refaktorco <refaktor.co@gmail.com>
2013-12-16 13:27:43 -0800

    -"Before Content" field for basic page content type.

--- templates/node.tpl.php
+++ templates/node.tpl.php
@@ -1,3 +1,7 @@
+<?php if (render($content['field_before_content'])) { print render($content['field_before_content']); } ?>
+<div class="clear break small"></div>
+
+
 <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <?php print $user_picture; ?>
@@ -19,6 +23,7 @@
       // We hide the comments and links now so that we can render them later.
       hide($content['comments']);
       hide($content['links']);
+      hide($content['field_before_content']);
       print render($content);
     ?>
   </div>
