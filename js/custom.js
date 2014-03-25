109a7d99c30b816ce9b5e297d2324a3957563ad1
refaktorco <refaktor.co@gmail.com>
2014-03-24 17:59:18 -0700

    -Fixed issue with custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,3 @@
-function header_scroll(){
-	if( jQuery(window).width() > 767 && !( jQuery('html').hasClass('ie9') ) ){
-	
-		jQuery('header#main').css({ 'min-height' : jQuery(window).height() });
-		
-		if( jQuery('header#main .header-wrap').length == 0 )
-			jQuery('header#main').wrapInner("<div class='header-wrap'></div>");
-		
-		var innerHeight = jQuery('.header-wrap').height();
-		var documentHeight = jQuery(document).height();
-		jQuery(window).scroll(function(){
-			if( jQuery(window).scrollTop() < documentHeight - innerHeight ){
-				jQuery('.header-wrap').css({ 'position' : 'fixed', 'top' : '0' });
-			} else {
-				jQuery('.header-wrap').css({ 'position' : 'absolute', 'top' : documentHeight - innerHeight });
-			}
-		});
-		
-	}
-}
-
 /*-----------------------------------------------------------------------------------*/
 /*	LOADER
 /*-----------------------------------------------------------------------------------*/
@@ -101,7 +80,7 @@ jQuery(document).ready(function($){
 /*-----------------------------------------------------------------------------------*/
 jQuery(window).load(function($){
 'use strict';
-	
+
 	var $container = jQuery('ul.grid');
 	
 	$container.isotope({
@@ -125,15 +104,16 @@ jQuery(window).load(function($){
 	
 	jQuery(window).trigger('resize');
 	
-	jQuery('header#main').height( jQuery(document).height() );
+	jQuery('header').height( jQuery(document).height() );
 	
 	jQuery(window).resize(function(){
-		jQuery('header#main').height( jQuery(window).height() );
+		jQuery('header').height( jQuery(window).height() );
 		setTimeout(function(){
-			jQuery('header#main').height( jQuery(document).height() );
+			jQuery('header').height( jQuery(document).height() );
 		}, 900);
 		
 		jQuery('#vertical, #vertical ul').height( jQuery(window).height() );
+
 	});
 	
 });
@@ -163,12 +143,85 @@ jQuery(document).ready(function($){
 'use strict';
 
 	$('.gallery.animate li').hover(function(){
-		$('.gallery li').not(this).stop().animate({ 'opacity' : '0.7' }, 200);
+		$('.gallery li').not(this).stop().animate({ 'opacity' : '0.3' }, 200);
 	}, function(){
 		$('.gallery li').stop().animate({ 'opacity' : '1' }, 200);
 	});
 });
-//*-----------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------*/
+/*	AJAX PORTFOLIO
+/*-----------------------------------------------------------------------------------*/
+jQuery(document).ready(function($){
+'use strict';
+	
+	$('body').on('click', 'ul.grid li a, .gallery-wrapper a', function(){
+		var url = $(this).attr('href');
+		
+		$('.content-wrapper').prepend('<div id="spinningSquaresG"><div id="spinningSquaresG_1" class="spinningSquaresG"></div><div id="spinningSquaresG_2" class="spinningSquaresG"></div><div id="spinningSquaresG_3" class="spinningSquaresG"></div><div id="spinningSquaresG_4" class="spinningSquaresG"></div><div id="spinningSquaresG_5" class="spinningSquaresG"></div><div id="spinningSquaresG_6" class="spinningSquaresG"></div><div id="spinningSquaresG_7" class="spinningSquaresG"></div><div id="spinningSquaresG_8" class="spinningSquaresG"></div></div>');
+		
+		$.get(url, function(data){
+			var filtered = jQuery(data).filter('section').removeClass('content');
+			
+			$(filtered).find(".rslides").responsiveSlides({
+			  speed: 500,
+			  timeout: 4000,
+			  pager: true
+			});
+			
+			$(filtered).imagesLoaded(function(){
+				$('ul.grid, .gallery-wrapper').animate({ 'left' : '-100%', 'opacity' : '0' }, function(){
+					$('ul.grid, .gallery-wrapper').css('max-height', '0px');
+					$("html, body").animate({ scrollTop: 0 }, 200);
+					$('#loader').html(filtered);
+					$('#spinningSquaresG').remove();
+					$('#loader').slideDown(function(){
+						jQuery(window).trigger('resize');
+					});
+				});
+			});
+		});
+		return false;
+	});
+	
+	$('body').on('click', '.post-nav', function(){
+		var url = $(this).attr('href');
+		
+		$('.content-wrapper').prepend('<div id="spinningSquaresG"><div id="spinningSquaresG_1" class="spinningSquaresG"></div><div id="spinningSquaresG_2" class="spinningSquaresG"></div><div id="spinningSquaresG_3" class="spinningSquaresG"></div><div id="spinningSquaresG_4" class="spinningSquaresG"></div><div id="spinningSquaresG_5" class="spinningSquaresG"></div><div id="spinningSquaresG_6" class="spinningSquaresG"></div><div id="spinningSquaresG_7" class="spinningSquaresG"></div><div id="spinningSquaresG_8" class="spinningSquaresG"></div></div>');
+		
+		$.get(url, function(data){
+			var filtered = jQuery(data).filter('section').removeClass('content');
+			
+			$(filtered).find(".rslides").responsiveSlides({
+			  speed: 500,
+			  timeout: 4000,
+			  pager: true
+			});
+			
+			$(filtered).imagesLoaded(function(){
+				$('#loader').animate({ 'left' : '-100%', 'opacity' : '0' }, function(){
+					$("html, body").animate({ scrollTop: 0 }, 200);
+					$('#spinningSquaresG').remove();
+					$('#loader').html(filtered).animate({ 'left' : '0', 'opacity' : '1' });
+				});
+			});
+		});
+		return false;
+	});
+	
+	$('body').on('click', 'a.close', function(){
+		
+		$('#loader').slideUp(function(){
+			$('ul.grid, .gallery-wrapper').css('max-height', '');
+			$('ul.grid, .gallery-wrapper').animate({ 'left' : '0', 'opacity' : '1' },function(){
+				jQuery(window).trigger('resize');
+			});
+		});
+		
+		return false;
+	});
+
+});
+/*-----------------------------------------------------------------------------------*/
 /*	VERTICAL GALLERY
 /*-----------------------------------------------------------------------------------*/
 jQuery(window).load(function($){
@@ -260,11 +313,48 @@ jQuery(document).ready(function($){
 
 });
 /*-----------------------------------------------------------------------------------*/
+/*	CONTACT FORM
+/*-----------------------------------------------------------------------------------*/
+jQuery(document).ready(function($){
+'use strict';
+
+	//CONTACT FORM
+		$('#contactform').submit(function(){
+	
+			var action = $(this).attr('action');
+	
+			$("#message").slideUp(750,function() {
+			$('#message').hide();
+	
+	 		$('#submit').attr('disabled','disabled');
+	
+			$.post(action, {
+				name: $('#name').val(),
+				email: $('#email').val(),
+				website: $('#website').val(),
+				comments: $('#comments').val()
+			},
+				function(data){
+					document.getElementById('message').innerHTML = data;
+					$('#message').slideDown('slow');
+					$('#submit').removeAttr('disabled');
+					if(data.match('success') != null) $('#contactform').slideUp('slow');
+					$(window).trigger('resize');
+				}
+			);
+	
+			});
+	
+			return false;
+	
+		});
+	
+});
+/*-----------------------------------------------------------------------------------*/
 /*	header#main
 /*-----------------------------------------------------------------------------------*/
 jQuery(window).load(function($){
 	
-	header_scroll();
 	center_items();
 	
 	jQuery(window).resize(function(){
@@ -277,5 +367,12 @@ jQuery(window).load(function($){
 		jQuery(this).find('.center').css('margin-top', parentHeight - thisHeight);
 	}, function(){
 		//nothing
+	});
+	
+	jQuery('.horizontal .center').each(function(){
+		var $this = jQuery(this);
+		var parentHeight = $this.parent().height() / 2;
+		var thisHeight = $this.height() / 2;
+		$this.css('margin-top', parentHeight - thisHeight);
 	});
 });
